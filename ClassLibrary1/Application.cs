942cf002b08d47ad03e6495516524e303f380ef7
kpc <kostroma.kpc@gmail.com>
2022-04-26 09:18:32 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Application
+    {
+        private static Application application { get; set; }
+        public List<TaskList> taskLists { get; set; }
+ 
+
+        /// <summary>
+        /// Возвращает ссылку на экземпляр приложения
+        /// </summary>
+        /// <returns></returns>
+        public Application GetApplication()
+        {
+            return application;
+        }
+        /// <summary>
+        /// Создает список задач и добавляет его в taskLists
+        /// </summary>
+        /// <param name="name"></param>
+        public void CreateTaskList(string name)
+        {
+            taskLists.Add(new TaskList());
+        }
+        /*public Application GetTaskListNames()
+        {
+            return TaskList.name;
+        }
+        */
+    }
+}
---
+++ ClassLibrary1/TaskList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class TaskList
+    {
+        public string name { get; set; }
+        public List<Task> tasks { get; set; }
+        /// <summary>
+        /// Возвращает имя списка задач
+        /// </summary>
+        /// <returns></returns>
+        public string GetName()
+        {
+            return name;
+        }
+        /// <summary>
+        /// Добавляет задачу в список задач
+        /// </summary>
+        /// <param name="task"></param>
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+        /// <summary>
+        /// Удаляет задачу из списка задач
+        /// </summary>
+        /// <param name="task"></param>
+        public void Remove(Task task)
+        {
+            tasks.Remove(task);
+        }
+        /// <summary>
+        /// Возвращает список всех задач
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+    }
+}
